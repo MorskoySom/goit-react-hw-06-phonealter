da5f48df02409273d3b7aa1374f219a3c60d276e
MorskoySom <Somovvitvit@gmail.com>
2023-11-02 21:14:12 +0200

    123

--- src/ContactsList/ContactsList.jsx
+++ src/ContactsList/ContactsList.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import { deleteContact } from '../Redux/contactsSlice';
+
 import { Contact } from "../Element/Element";
 
 export const ContactList = () => {
     const persons = useSelector(state => state.contacts.data);
-    const dispatch = useDispatch();
-
-    const handleDeleteContact = (elementId) => {
-        dispatch(deleteContact(elementId));
-    }
 
     console.log(persons)
-
     return (
         <ul>
             {persons.map(person => (
                 <li key={person.id}>
-                    <Contact info={person} toDelete={handleDeleteContact} />
+                    <Contact info={person} />
                 </li>
             ))}
         </ul>
